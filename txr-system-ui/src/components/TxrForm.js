2a542c5948927c9fb7b450994171c965babaf176
Nag-Training <nag-training@NAGABHUSHANAMs-MacBook-Pro.local>
2019-09-05 11:10:31 +0530

    Txr-system

---
+++ txr-system-ui/src/components/TxrForm.js
@@ -0,0 +1,57 @@
+
+import React, { useState } from 'react';
+
+const TxrForm = (props) => {
+    const [amount, setAmount] = useState('')
+    const [fromAccNumber, setFromAccNumber] = useState('')
+    const [toAccNumber, setToAccNumber] = useState('')
+    const handleSubmit = e => {
+        e.preventDefault();
+        const formData = { amount, fromAccNumber, toAccNumber }
+        console.log(formData);
+    };
+    return (
+        <div className="row">
+            <div className="col-sm-5 col-md-5">
+                <div className="card">
+                    <div className="card-header">Txr Form</div>
+                    <div className="card-body">
+                        <form onSubmit={e => handleSubmit(e)}>
+                            <div className="form-group">
+                                <label htmlFor="fromAccNumber">Amount</label>
+                                <input
+                                    id="amount"
+                                    type="number"
+                                    className="form-control"
+                                    onChange={e => setAmount(e.target.value)}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="fromAccNumber">From Account</label>
+                                <input
+                                    id="fromAccNumber"
+                                    type="number"
+                                    className="form-control"
+                                    onChange={e => setFromAccNumber(e.target.value)}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="toAccNumber">To Account</label>
+                                <input
+                                    id="toAccNumber"
+                                    type="number"
+                                    className="form-control"
+                                    onChange={e => setToAccNumber(e.target.value)}
+                                />
+                            </div>
+                            <button className="btn btn-dark">Txr</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default TxrForm;
